4dded1475f842d93dd035992a9ae7e35cf4208c9
Luciana Soressi <lsoressi@gmail.com>
2024-02-08 17:26:22 -0300

    Ej de Tipos de Datos y Operadores

---
+++ Clase 2/TiposDeDatosyOperadores.py
@@ -0,0 +1,74 @@
+
+# numerico - int (numero entero, sin comas)
+# numEntero = 25
+
+# numerico - float (numero con comas, con decimales)
+# numFloat = 29.4
+
+# boolean - (verdadero/falso, 1 y 0)
+# esCliente = True
+
+# Texto - String (Cadena de Caracteres)
+# saludo = 'Hola Mundo!'
+
+
+
+# Operadores
+# Aritmeticos
+
+num1 = 10
+num2 = 7
+
+sum = num1 + num2
+print(sum)
+
+resta = num1 - num2
+print(resta)
+
+mult = num1 * num2
+print(mult)
+
+div = num1/num2
+print(div)
+
+raizCuadrada = num1 **3
+print(raizCuadrada)
+
+# Modulo nos dice si es Par o Impar un numero, 1 para numero par y 0 para impar
+# Division entera, redondea el resultado para donde sea necesario devolviendo un resultado entero
+
+
+divisionEntera = num1//num2
+print(divisionEntera)
+
+# Asignacion = 'ESTA VARIABLE ES !!! ESTE DATO'
+variable = "Hola"
+# Esta variable es el string Hola
+
+# Comparacion = Esta variable es igual a este dato? Devuelve un booleano (True/False).
+print(variable == "CHAU")
+
+# Distinto que, lo opuesto a "igual que"
+print(variable != "CHAU") #True , es verdad que es dintinto a Chau
+
+
+
+
+edad = 14
+
+print (edad == 18)   #igual que
+print (edad != 18)   #distinto que
+print (edad > 18)    #mayor que
+print (edad >= 18)   #mayor o igual que
+print (edad < 18)    #menor que
+print (edad <= 18)   #menor o igual que
+#Todos devuelven booleanos
+
+edad = 14
+print (edad)
+edad = edad + 1
+print (edad)
+edad += 1   #Incremeto
+print(edad) 
+edad -= 1  #Decremento
+print(edad) 
