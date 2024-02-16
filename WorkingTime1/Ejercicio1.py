5644f0acc23254c6769ee07b1e092542b1a9f3ea
Luciana Soressi <lsoressi@gmail.com>
2024-02-16 17:28:13 -0300

    Inicie en 0 el contador y movi de lugar el incremental

--- WorkingTime1/Ejercicio1.py
+++ WorkingTime1/Ejercicio1.py
@@ -1,10 +1,9 @@
-contador = 1
+contador = 0
 acumulador = 0
-while contador <= 100:    
+while contador <= 100:
+    contador +=1    
     print('Valor del Contador', contador)
     print('Valor del Acumulador', acumulador)
 
     acumulador = (contador + acumulador)  
     print('acumulador', acumulador)
-    
-    contador +=1
