c5ffc497b0cc9caeb7100e63aff3e40abd0b5042
Luciana Soressi <lsoressi@gmail.com>
2024-04-08 20:49:30 -0300

    forms py con todos los campos

--- StockControl/Compra/forms.py
+++ StockControl/Compra/forms.py
@@ -6,13 +6,14 @@ class proveedor_Formulario(forms.ModelForm):
 
     class Meta:
         model = Proveedor
-        fields = '__all__'
-        #fields = ['rSocial', 'domicilio', 'cuit']
+        #fields = '__all__'
+        fields = ['rSocial', 'domicilio', 'cuit']
 
 class producto_Formulario(forms.ModelForm):
 
     class Meta:
         model = Producto
-        fields = '__all__'
+        #fields = '__all__'
+        fields = ['nombre', 'precio', 'stock_actual','proveedor']
        
 
